8c0e45441bcbcfc73e94d29443e8a5d4d274e5e0
Rádi Olivér <radi.oliver@diak.szbi-pg.hu>
2025-10-20 08:54:23 +0200

    adf

--- Program.cs
+++ Program.cs
@@ -1,194 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace uzenetrogzito
 {
-    class Hivo
-    {
-        public int telefonSzam { get; set; }
-        public string nev { get; set; }
-        public string uzenet { get; set; }
-        public DateTime datum { get; set; }
-
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-
             int sorszam1 = 0;
             int sorszam2 = 0;
             int sorszam3 = 0;
             int sorszam4 = 0;
-            StreamWriter sw = new StreamWriter("../../titkarsag.txt", true);
-            sw.Write("---------Titkárság üzenetei---------\n");
-            StreamWriter sw2 = new StreamWriter("../../tanari.txt", true);
-            sw.Write("---------Tanári üzenetei---------\n");
-            StreamWriter sw3 = new StreamWriter("../../kollegium.txt", true); 
-            sw.Write("---------Kollégium üzenetei---------\n");
-            StreamWriter sw4 = new StreamWriter("../../konyvtar.txt", true);
-            sw.Write("---------Könyvtár üzenetei---------\n");
 
+           
+            File.WriteAllText("../../titkarsag.txt", "---------Titkárság üzenetei---------\n");
+            File.WriteAllText("../../tanari.txt", "---------Tanári üzenetei---------\n");
+            File.WriteAllText("../../kollegium.txt", "---------Kollégium üzenetei---------\n");
+            File.WriteAllText("../../konyvtar.txt", "---------Könyvtár üzenetei---------\n");
 
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine("---------------------");
                 Console.WriteLine(" PG - ÜZENETRÖGZÍTŐ");
                 Console.WriteLine("---------------------");
 
-
                 Console.WriteLine("11 - titkárság\n14 - tanári\n15 - kollégium\n16 - könyvtár");
-                Console.Write("\nVálasszon a fenti lehetőségek közül:");
-                int valasztott = int.Parse(Console.ReadLine());
+                Console.Write("\nVálasszon a fenti lehetőségek közül: ");
+                if (!int.TryParse(Console.ReadLine(), out int valasztott))
+                {
+                    Console.WriteLine("Hibás bemenet. Nyomjon egy gombot a folytatáshoz...");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                string nev, uzenet, datum, telefonSzam;
+                Random rnd = new Random();
+
+                Console.Clear();
+                Console.Write("\nKérem adja meg a nevét: ");
+                nev = Console.ReadLine();
+                Console.Write("Kérem adja meg az üzenetet: ");
+                uzenet = Console.ReadLine();
+                datum = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss");
+                telefonSzam = "+36" + rnd.Next(100000000, 900000000).ToString();
+
+                string sor = "";
 
                 switch (valasztott)
                 {
-
-
                     case 11:
-                        
-                        Console.Clear();
-                        Random rnd = new Random();
-                        Console.Write("\nKérem adja meg a nevét: ");
-                        string nev = Console.ReadLine();
-                        Console.Write("Kérem adja meg az üzenetet: ");
-                        string uzenet = Console.ReadLine();
-                        string datum = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss");
-                        string telefonSzam = "+36" + rnd.Next(100000000, 900000000).ToString();
-                          
-                        sorszam1 = sorszam1 + 1;
-                        sw.WriteLine($"{sorszam1};{uzenet};{nev};{telefonSzam};{datum}");
-                        Console.Write("Szeretne mégegy üzenetet hagyni? (i/n)");
-                        sw.Close();
-                        string valasz = Console.ReadLine();
-                        if (valasz == "i")
+                        sorszam1++;
+                        sor = $"{sorszam1};{uzenet};{nev};{telefonSzam};{datum}";
+                        using (StreamWriter sw = File.AppendText("../../titkarsag.txt"))
                         {
-                            Console.Clear();
-                            break;
+                            sw.WriteLine(sor);
                         }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Viszontlátásra!");
-                            return;
-                        }
-
+                        break;
 
                     case 14:
-                        Console.Clear();
-                        Random rnd2 = new Random();
-                        Console.Write("\nKérem adja meg a nevét: ");
-                        string nev2 = Console.ReadLine();
-                        Console.Write("Kérem adja meg az üzenetet: ");
-                        string uzenet2 = Console.ReadLine();
-                        string datum2 = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss");
-                        string telefonSzam2 = "+36" + rnd2.Next(100000000, 900000000).ToString();
-
-                        sorszam2 = sorszam2 + 1;
-                        sw2.WriteLine($"{sorszam2};{uzenet2};{nev2};{telefonSzam2};{datum2}");
-                        sw2.Close();
-                        Console.Write("Szeretne mégegy üzenetet hagyni? (i/n)");                        
-                        string valasz2 = Console.ReadLine();
-                        if (valasz2 == "i")
+                        sorszam2++;
+                        sor = $"{sorszam2};{uzenet};{nev};{telefonSzam};{datum}";
+                        using (StreamWriter sw2 = File.AppendText("../../tanari.txt"))
                         {
-                            Console.Clear();
-                            break;
+                            sw2.WriteLine(sor);
                         }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Viszontlátásra!");
-                            return;
-                        }
+                        break;
 
                     case 15:
-                        Console.Clear();
-                        Random rnd3 = new Random();
-                        Console.Write("\nKérem adja meg a nevét: ");
-                        string nev3 = Console.ReadLine();
-                        Console.Write("Kérem adja meg az üzenetet: ");
-                        string uzenet3 = Console.ReadLine();
-                        string datum3 = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss");
-                        string telefonSzam3 = "+36" + rnd3.Next(100000000, 900000000).ToString();
-
-                        sorszam3 = sorszam3 + 1;
-                        sw3.WriteLine($"{sorszam3};{uzenet3};{nev3};{telefonSzam3};{datum3}");
-                        sw3.Close();
-                        Console.Write("Szeretne mégegy üzenetet hagyni? (i/n)");
-                        string valasz3 = Console.ReadLine();
-                        if (valasz3 == "i")
+                        sorszam3++;
+                        sor = $"{sorszam3};{uzenet};{nev};{telefonSzam};{datum}";
+                        using (StreamWriter sw3 = File.AppendText("../../kollegium.txt"))
                         {
-                            Console.Clear();
-                            break;
+                            sw3.WriteLine(sor);
                         }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Viszontlátásra!");
-                            return;
-                        }
+                        break;
 
                     case 16:
-                        Console.Clear();
-                        Random rnd4 = new Random();
-                        Console.Write("\nKérem adja meg a nevét: ");
-                        string nev4 = Console.ReadLine();
-                        Console.Write("Kérem adja meg az üzenetet: ");
-                        string uzenet4 = Console.ReadLine();
-                        string datum4 = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss");
-                        string telefonSzam4 = "+36" + rnd4.Next(100000000, 900000000).ToString();
-
-                        sorszam4 = sorszam4 + 1;
-                        sw4.WriteLine($"{sorszam4};{uzenet4};{nev4};{telefonSzam4};{datum4}");
-                        sw4.Close();
-                        Console.Write("Szeretne mégegy üzenetet hagyni? (i/n)");
-                        string valasz4 = Console.ReadLine();
-                        if (valasz4 == "i")
+                        sorszam4++;
+                        sor = $"{sorszam4};{uzenet};{nev};{telefonSzam};{datum}";
+                        using (StreamWriter sw4 = File.AppendText("../../konyvtar.txt"))
                         {
-                            Console.Clear();
-                            break;
+                            sw4.WriteLine(sor);
                         }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Viszontlátásra!");
-                            return;
-                        }
+                        break;
 
+                    default:
+                        Console.WriteLine("Nincs ilyen kategória. Nyomjon egy gombot a folytatáshoz...");
+                        Console.ReadKey();
+                        continue;
+                }
 
+                Console.Write("\nSzeretne mégegy üzenetet hagyni? (i/n): ");
+                string valasz = Console.ReadLine().ToLower();
+                if (valasz == "n")
+                {
+                    Console.WriteLine("Köszönjük hívását!\nViszont hallásra!");
+                    Console.ReadKey();
+                    break;
                 }
             }
-
-
-
-
-
-
-
-
-        }
-         static void uzenetRogzito()
-        {
-
-            Random rnd = new Random();
-            Console.Write("\nKérem adja meg a nevét: ");
-            string nev = Console.ReadLine();
-            Console.Write("Kérem adja meg az üzenetet: ");
-            string uzenet = Console.ReadLine();
-            string datum = DateTime.Now.ToString("yyyy.MM.dd HH:mm");
-            StreamWriter sw = new StreamWriter("uzenetek.txt", true);
-
-
-        }
-        static void telefonSzam()
-        {
-            string telefonszam = "";
-
         }
     }
 }
--- uzenetVisszanezo/Program.cs
+++ uzenetVisszanezo/Program.cs
@@ -11,9 +11,99 @@ namespace uzenetVisszanezo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" PG - ÜZENETVISSZANÉZŐ");
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("11 - titkárság\n14 - tanári\n15 - kollégium\n16 - könyvtár\n0 - összes");
+            Console.Write("\nVálasszon a fenti lehetőségek közül: ");
+            int valasztott = int.Parse(Console.ReadLine());
             
-
-
+            if (valasztott == 0)
+            {
+                List<string> strings = new List<string>();
+                StreamReader sr = new StreamReader("../../../titkarsag.txt");
+                foreach (var item in File.ReadLines("../../../titkarsag.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                sr = new StreamReader("../../../tanari.txt");
+                foreach (var item in File.ReadLines("../../../tanari.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                sr = new StreamReader("../../../kollegium.txt");
+                foreach (var item in File.ReadLines("../../../kollegium.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                sr = new StreamReader("../../../konyvtar.txt");
+                foreach (var item in File.ReadLines("../../../konyvtar.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                foreach (var item in strings)
+                {
+                    string[] adatok = item.Split(';');
+                    Console.WriteLine($"Sorszám: {adatok[0]}\nÜzenet: {adatok[1]}\nNév: {adatok[2]}\nTelefonszám: {adatok[3]}\nDátum: {adatok[4]}\n------------------");
+                }
+            }
+            else if (valasztott == 11)
+            {
+                List<string> strings = new List<string>();
+                StreamReader sr = new StreamReader("../../../titkarsag.txt");
+                foreach (var item in File.ReadLines("../../../titkarsag.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                foreach (var item in strings)
+                {
+                    string[] adatok = item.Split(';');
+                    Console.WriteLine($"Sorszám: {adatok[0]}\nÜzenet: {adatok[1]}\nNév: {adatok[2]}\nTelefonszám: {adatok[3]}\nDátum: {adatok[4]}\n------------------");
+                }
+            }
+            else if (valasztott == 14)
+            {
+                List<string> strings = new List<string>();
+                StreamReader sr = new StreamReader("../../../tanari.txt");
+                foreach (var item in File.ReadLines("../../../tanari.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                foreach (var item in strings)
+                {
+                    string[] adatok = item.Split(';');
+                    Console.WriteLine($"Sorszám: {adatok[0]}\nÜzenet: {adatok[1]}\nNév: {adatok[2]}\nTelefonszám: {adatok[3]}\nDátum: {adatok[4]}\n------------------");
+                }
+            }
+            else if (valasztott == 15)
+            {
+                List<string> strings = new List<string>();
+                StreamReader sr = new StreamReader("../../../kollegium.txt");
+                foreach (var item in File.ReadLines("../../../kollegium.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                foreach (var item in strings)
+                {
+                    string[] adatok = item.Split(';');
+                    Console.WriteLine($"Sorszám: {adatok[0]}\nÜzenet: {adatok[1]}\nNév: {adatok[2]}\nTelefonszám: {adatok[3]}\nDátum: {adatok[4]}\n------------------");
+                }
+            }
+            else if (valasztott == 16)
+            {
+                List<string> strings = new List<string>();
+                StreamReader sr = new StreamReader("../../../konyvtar.txt");
+                foreach (var item in File.ReadLines("../../../konyvtar.txt").Skip(1))
+                {
+                    strings.Add(item);
+                }
+                foreach (var item in strings)
+                {
+                    string[] adatok = item.Split(';');
+                    Console.WriteLine($"Sorszám: {adatok[0]}\nÜzenet: {adatok[1]}\nNév: {adatok[2]}\nTelefonszám: {adatok[3]}\nDátum: {adatok[4]}\n------------------");
+                }
+            }
 
 
 
